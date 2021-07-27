21c0a8c5ffdc368794b289099b5d700fb5ab76aa
J4vakhishvili <n1kc0nnyt@gmail.com>
2021-07-27 11:35:18 +0400

    Update categories.js

--- assets/script/categories.js
+++ assets/script/categories.js
@@ -20,10 +20,8 @@ function myFunction() {
 
 function openNav() {
     document.getElementById("mySidenav").style.width = "350px";
-    document.getElementById("main").style.marginLeft = "350px";
 }
   
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft= "0";
 }
