410e0646a7cafbb2852aad127a74f7ba21192a79
J4vakhishvili <n1kc0nnyt@gmail.com>
2021-07-27 11:32:49 +0400

    many changes
    შეიცვალა კატეგორიები, გაკეთდა რესფონსივი, გასწორდა ხარვეზები

--- assets/script/categories.js
+++ assets/script/categories.js
@@ -15,3 +15,15 @@ function myFunction() {
         }
     }
 }
+
+//open
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "350px";
+    document.getElementById("main").style.marginLeft = "350px";
+}
+  
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+}
